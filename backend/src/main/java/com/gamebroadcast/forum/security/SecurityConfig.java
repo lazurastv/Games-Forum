3d14b78d3a129684edd330c9d07722b669debb1f
Hubert Piłka <hubert.pilka@outlook.com>
2022-02-14 00:27:22 +0100

    Add basic auth

---
+++ backend/src/main/java/com/gamebroadcast/forum/security/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.gamebroadcast.forum.security;
+
+import static com.gamebroadcast.forum.user.Role.ADMIN;
+import static com.gamebroadcast.forum.user.Role.USER;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public SecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/api/**").hasAnyRole(ADMIN.name())
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails u1 = User.builder().username("u").password(passwordEncoder.encode("p")).roles(USER.name()).build();
+        UserDetails u2 = User.builder().username("a").password(passwordEncoder.encode("p")).roles(ADMIN.name()).build();
+
+        return new InMemoryUserDetailsManager(u1, u2);
+    }
+}
