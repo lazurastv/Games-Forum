599b3cd7fce7ec7958f78d3eea07a406aba6edf6
Hubert Piłka <hubert.pilka@outlook.com>
2022-02-20 16:32:31 +0100

    Refactor AppUser with lombok

--- backend/src/main/java/com/gamebroadcast/forum/user/AppUser.java
+++ backend/src/main/java/com/gamebroadcast/forum/user/AppUser.java
@@ -18,6 +18,13 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "app_user", uniqueConstraints = {
         @UniqueConstraint(name = "user_unique_username", columnNames = "username"),
@@ -57,63 +64,6 @@ public class AppUser implements UserDetails {
     @Column(name = "last_used", nullable = false)
     private Timestamp lastUsed;
 
-    public AppUser(Long id, String username, String email, String password, String shortDescription,
-            String profilePicturePath, String role, boolean enabled, boolean locked, Timestamp lastUsed) {
-        this.id = id;
-        this.username = username;
-        this.email = email;
-        this.password = password;
-        this.shortDescription = shortDescription;
-        this.profilePicturePath = profilePicturePath;
-        this.role = role;
-        this.enabled = enabled;
-        this.locked = locked;
-        this.lastUsed = lastUsed;
-    }
-
-    public AppUser() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getShortDescription() {
-        return shortDescription;
-    }
-
-    public void setShortDescription(String shortDescription) {
-        this.shortDescription = shortDescription;
-    }
-
-    public String getProfilePicturePath() {
-        return profilePicturePath;
-    }
-
-    public void setProfilePicturePath(String profilePicturePath) {
-        this.profilePicturePath = profilePicturePath;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return getAuthoritiesFromRole();
@@ -127,24 +77,6 @@ public class AppUser implements UserDetails {
     }
 
     @Override
-    public String getPassword() {
-        return this.password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public String getUsername() {
-        return this.username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    @Override
     public boolean isAccountNonExpired() {
         return true;
     }
@@ -154,22 +86,9 @@ public class AppUser implements UserDetails {
         return !this.locked;
     }
 
-    public void setAccountNonLocked(boolean nonLocked) {
-        this.locked = !nonLocked;
-    }
-
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return this.enabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
     }
 
 }
