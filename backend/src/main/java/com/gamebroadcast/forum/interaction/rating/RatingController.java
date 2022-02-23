49c6d09452bc4dafb85fd4eeffcaa7d53a7fab38
Norsien <smola27ms@gmail.com>
2022-02-23 16:52:47 +0100

    rating bez sprawdzania gier

---
+++ backend/src/main/java/com/gamebroadcast/forum/interaction/rating/RatingController.java
@@ -0,0 +1,95 @@
+package com.gamebroadcast.forum.interaction.rating;
+
+import java.util.List;
+
+import com.gamebroadcast.forum.exceptions.ApiRequestException;
+import com.gamebroadcast.forum.exceptions.NoEditRightsException;
+import com.gamebroadcast.forum.interaction.rating.models.RatingAdd;
+import com.gamebroadcast.forum.interaction.rating.models.RatingUpdate;
+import com.gamebroadcast.forum.interaction.rating.models.RatingVM;
+import com.gamebroadcast.forum.security.SessionUtils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping(path = "api/rating")
+public class RatingController {
+	
+	@Autowired
+	private RatingService ratingService;
+
+	@GetMapping
+	public List<RatingVM> getAll() {
+		return ratingService.getAll();
+	}
+
+	@GetMapping(path = "/GameId/{id}")
+    public List<RatingVM> getByGameId(@PathVariable("id") Long id) {
+        return ratingService.getByGameId(id);
+    }
+
+    @GetMapping(path = "/UserId/{id}")
+    public List<RatingVM> getByUserId(@PathVariable("id") Long id) {
+        return ratingService.getByUserId(id);
+    }
+
+    @GetMapping(path = "/Id/{id}")
+    public RatingVM getById(@PathVariable("id") Long id) {
+        try {
+            return ratingService.get(id);
+        } catch (RuntimeException e) {
+            throw new ApiRequestException(e.getMessage());
+        }
+    }
+
+    @PostMapping
+    @PreAuthorize("hasRole('USER')")
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public void add(@RequestBody RatingAdd ratingAdd) {
+        try {
+            ratingService.add(ratingAdd);
+        } catch (RuntimeException e) {
+            throw new ApiRequestException(e.getMessage());
+        }
+    }
+
+    @PutMapping(path = "/{id}")
+    @PreAuthorize("hasRole('USER')")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void update(@PathVariable("id") Long id, @RequestBody RatingUpdate ratingUpdate) {
+        try {
+            if (!sessionUserCanEditRating(id)) {
+                throw new NoEditRightsException("Rating");
+            }
+            ratingService.update(id, ratingUpdate);
+        } catch (RuntimeException e) {
+            throw new ApiRequestException(e.getMessage());
+        }
+    }
+
+    @DeleteMapping(path = "/{id}")
+    @PreAuthorize("hasRole('USER')")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable("id") Long id) {
+        try {
+            if (!sessionUserCanDeleteRating(id)) {
+                throw new NoEditRightsException("Rating");
+            }
+            ratingService.delete(id);
+        } catch (RuntimeException e) {
+            throw new ApiRequestException(e.getMessage());
+        }
+    }
+
+    private boolean sessionUserCanEditRating(Long id) {
+        return ratingService.sessionUserIsOwner(id);
+    }
+
+    private boolean sessionUserCanDeleteRating(Long id) {
+        return ratingService.sessionUserIsOwner(id) || SessionUtils.getUserFromSession().getRole().equals("ADMIN");
+    }
+}
---
+++ backend/src/main/java/com/gamebroadcast/forum/interaction/rating/RatingService.java
@@ -0,0 +1,70 @@
+package com.gamebroadcast.forum.interaction.rating;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import com.gamebroadcast.forum.article.ArticleService;
+import com.gamebroadcast.forum.exceptions.ItemNotFoundException;
+import com.gamebroadcast.forum.interaction.rating.models.Rating;
+import com.gamebroadcast.forum.interaction.rating.models.RatingAdd;
+import com.gamebroadcast.forum.interaction.rating.models.RatingUpdate;
+import com.gamebroadcast.forum.interaction.rating.models.RatingVM;
+import com.gamebroadcast.forum.security.SessionUtils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RatingService {
+    @Autowired
+    private RatingRepository ratingRepository;
+
+    @Autowired
+    private ArticleService articleService;
+
+    public List<RatingVM> getAll() {
+        List<Rating> ratings = ratingRepository.findAll();
+        return RatingVM.toRatingVMList(ratings);
+    }
+    
+    public List<RatingVM> getByGameId(Long id) {
+        List<Rating> ratings = ratingRepository.findByGameId(id);
+        return RatingVM.toRatingVMList(ratings);
+    }
+
+    public List<RatingVM> getByUserId(Long id) {
+        List<Rating> ratings = ratingRepository.findByUserId(id);
+        return RatingVM.toRatingVMList(ratings);
+    }
+
+    public RatingVM get(Long id) throws IllegalStateException {
+        Rating rating = getRating(id);
+        return new RatingVM(rating);
+    }
+
+    public void add(RatingAdd ratingAdd) throws IllegalStateException {
+        Rating rating = ratingAdd.toRating(articleService);
+        ratingRepository.save(rating);
+    }
+
+    @Transactional
+    public void update(Long id, RatingUpdate ratingUpdate) throws IllegalStateException {
+        Rating rating = getRating(id);
+        ratingUpdate.update(rating);
+    }
+
+    public void delete(Long id) throws IllegalStateException {
+        Rating rating = getRating(id);
+        ratingRepository.delete(rating);
+    }
+
+    public boolean sessionUserIsOwner(Long id) {
+        return getRating(id).ownedBy(SessionUtils.getUserFromSession());
+    }
+
+    public Rating getRating(Long id) {
+        return ratingRepository.findById(id)
+                .orElseThrow(() -> new ItemNotFoundException("Rating", id));
+    }
+}
