64c2a109ad88b42ffc806170a54db6f9bdeb1c1a
Dominik Wawrzyniuk <dominikwawrzyniuk@gmail.com>
2022-04-07 23:20:05 +0200

    Added getSessionUser method, fixed bug with login crash

--- backend/src/main/java/com/gamebroadcast/forum/user/UserController.java
+++ backend/src/main/java/com/gamebroadcast/forum/user/UserController.java
@@ -1,5 +1,24 @@
 package com.gamebroadcast.forum.user;
 
+import com.gamebroadcast.forum.user.models.UserVM;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping(path = "api/user")
+@RequiredArgsConstructor
 public class UserController {
 
+    private final UserService userService;
+
+    @GetMapping(path = "/session")
+    @PreAuthorize("hasRole('USER')")
+    public UserVM getSessionUser() {
+        return userService.getSessionUser();
+    }
 }
