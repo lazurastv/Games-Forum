5ec530056d9aac6149ee5361d1035ee5301ef3ab
Barteq3012 <bartek.cz3012@gmail.com>
2022-04-16 15:39:03 +0200

    Trying to display articles and reviews

--- frontend/forum-graczy/src/pages/Articles/Articles.tsx
+++ frontend/forum-graczy/src/pages/Articles/Articles.tsx
@@ -1,15 +1,36 @@
 import Container from '@mui/material/Container';
-import { articles } from '../../data-mock/articles';
 import ArticleItem from './ArticleItem';
-
+import { ArticleControllerApi } from '../../api/api/apis/ArticleControllerApi';
+import { ArticleVM } from '../../api/api';
+import { useEffect, useState } from 'react';
 
 const Articles = () => {
+  const [articlesArray, setArticlesArray] = useState<ArticleVM[]>([]);
+  const articles = new ArticleControllerApi();
+  useEffect( () => {
+  articles
+    .getAllArticles()
+    .then((result) => {
+      setArticlesArray(result);
+    })
+    .catch((error) => console.error("Read" + error));
+  })
 
+  //co z obrazkami?
+  // string | undefined as string - czy to jest poprawnie czy dodać undefined do interfejsu, czy te dane mogą być undefined?
   return (
     <Container maxWidth="xl">
-      {articles.map((a, idx) => 
-      <ArticleItem key={idx} date={a.date} title={a.title} author={a.author} image={a.image}/>
-      )}
+      {articlesArray.map((a, idx) => (
+        <ArticleItem
+          key={idx}
+          date={(a.publishDate as Date).toLocaleString().replace('/', '.').split(',')[0]}
+          title={a.title as string}
+          author={a.authorName as string}
+          image={
+            "https://cdn.mos.cms.futurecdn.net/3ZbPC5LNRVccsePfX2PbM7-1200-80.jpg"
+          }
+        />
+      ))}
     </Container>
   );
 };
--- frontend/forum-graczy/src/pages/Reviews/Reviews.tsx
+++ frontend/forum-graczy/src/pages/Reviews/Reviews.tsx
@@ -1,14 +1,35 @@
 import Container from '@mui/material/Container';
-import ReviewItem from './ReviewItem';
-import { reviews } from '../../data-mock/reviews';
+import ReviewItem from './ReviewItem'
+import { useEffect, useState } from "react";
+import { ReviewControllerApi } from '../../api/api/apis/ReviewControllerApi';
+import { ReviewVM } from '../../api/api/models/ReviewVM';
 
 const Reviews = () => {
-
+  const [reviewsArray, setReviewsArray] = useState<ReviewVM[]>([]);
+  const reviews = new ReviewControllerApi();
+  useEffect(() => {
+    reviews
+      .getAllReviews()
+      .then((result) => {
+        setReviewsArray(result);
+      })
+      .catch((error) => console.error("Read" + error));
+  });
   return (
     <Container maxWidth="xl">
-      {reviews.map((r, idx) =>
-      <ReviewItem key={idx} date={r.date} title={r.title} content={r.content} author={r.author} rate={r.rate} image={r.image}/>
-      )}
+      {reviewsArray.map((r, idx) => (
+        <ReviewItem
+          key={idx}
+          date={(r.publishDate as Date).toLocaleString().replace('/', '.').split(',')[0]}
+          title={r.title as string}
+          content={"xd"}
+          author={r.authorName as string}
+          rate={6}
+          image={
+            "https://cdn.mos.cms.futurecdn.net/3ZbPC5LNRVccsePfX2PbM7-1200-80.jpg"
+          }
+        />
+      ))}
     </Container>
   );
 };
