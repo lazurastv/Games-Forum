2997a9ca8f08cf730c34519a22d0596905534c55
Piotr Nowak <npiotrek44@gmail.com>
2022-04-18 09:31:54 +0200

    Walidacja formularza dodania artykułu

--- frontend/src/pages/CreateContent/CreateArticle/CreateArticle.tsx
+++ frontend/src/pages/CreateContent/CreateArticle/CreateArticle.tsx
@@ -1,10 +1,9 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { EditorState } from "draft-js";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
-import { Button, TextField } from "@mui/material";
-import { styled } from "@mui/material/styles";
+import { Button } from "@mui/material";
 import SectionHeader from "../../../components/SectionHeader";
 import { loadArticle, uploadArticle } from "./fetchArticles";
 import DraftEditor from "../../../components/Editor/DraftEditor";
@@ -32,36 +31,42 @@ export default function CreateArticle() {
   return (
     <Container maxWidth="lg" sx={{ my: 4 }}>
       <SectionHeader>Napisz artykuł</SectionHeader>
-      <Box sx={{ mb: 4 }}>
-        <OneLineInput
-          formLabel="Tytuł"
-          placeholder="Napisz tytuł..."
-          value={title}
-          onChange={(e: any) => setTitle(e.target.value)}
+      <Box component="form" onSubmit={(e: any) => e.preventDefault()}>
+        <Box sx={{ mb: 4 }}>
+          <OneLineInput
+            formLabel="Tytuł"
+            placeholder="Napisz tytuł..."
+            value={title}
+            onChange={(e: any) => setTitle(e.target.value)}
+          />
+          <OneLineInput
+            formLabel="Wprowadzenie"
+            placeholder="Napisz wprowadzenie..."
+            value={introduction}
+            onChange={(e: any) => setIntroduction(e.target.value)}
+          />
+        </Box>
+        <DraftEditor
+          editorState={editorState}
+          setEditorState={setEditorState}
         />
-        <OneLineInput
-          formLabel="Wprowadzenie"
-          placeholder="Napisz wprowadzenie..."
-          value={introduction}
-          onChange={(e: any) => setIntroduction(e.target.value)}
-        />
-      </Box>
-      <DraftEditor editorState={editorState} setEditorState={setEditorState} />
-      <Box sx={{ textAlign: "right" }}>
-        <Button
-          sx={{
-            width: {
-              sm: "100%",
-              md: "150px",
-            },
-          }}
-          variant="contained"
-          color="secondary"
-          size="large"
-          onClick={handleSave}
-        >
-          Zapisz
-        </Button>
+        <Box sx={{ textAlign: "right" }}>
+          <Button
+            sx={{
+              width: {
+                sm: "100%",
+                md: "150px",
+              },
+            }}
+            type="submit"
+            variant="contained"
+            color="secondary"
+            size="large"
+            onClick={handleSave}
+          >
+            Zapisz
+          </Button>
+        </Box>
       </Box>
     </Container>
   );
