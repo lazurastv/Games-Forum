60ef322251e08d264ac0a28b5f29bdefd96c3164
Prz-mek <kwasniewski.pj@gmail.com>
2022-05-21 15:55:54 +0200

    404 Error

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -5,6 +5,7 @@ import Carousel from "../../components/Carousel/Carousel";
 import GameTile from "../../components/Tile/GameTile";
 import { gamesCarousel, reviewsCarousel } from "../../data-mock/carousels";
 import SectionHeader from "../../components/SectionHeader";
+import PageNotFoundError from "../../components/Errors/PageNotFoundError";
 
 export default function Home() {
   return (
