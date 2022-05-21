00a774bc1ec98b6bf948d4f7e16188b8e531b42d
Dominik Wawrzyniuk <dominikwawrzyniuk@gmail.com>
2022-05-21 12:31:53 +0200

    Added register method

--- frontend/src/api/api/apis/AuthApi.ts
+++ frontend/src/api/api/apis/AuthApi.ts
@@ -5,6 +5,28 @@ import * as runtime from '../runtime';
  * 
  */
 export class AuthApi extends runtime.BaseAPI {
+    async register(username: string, email: string, password: string) {
+        const headerParameters: runtime.HTTPHeaders = {};
+
+        headerParameters['Content-Type'] = 'application/json';
+
+        const response = await this.request({
+            path: `/api/user/register`,
+            method: 'POST',
+            headers: headerParameters,
+
+            body: {
+                username: username,
+                email: email,
+                password: password
+            },
+        }, {
+            mode: 'cors'
+        });
+
+        return response;
+    }
+
     async login(username: string, password: string) {
         const queryParameters: any = {};
 
