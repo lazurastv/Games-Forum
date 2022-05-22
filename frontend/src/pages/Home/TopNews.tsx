30b57a6981392b92a77d5040e21457d4e197b582
Dominik Wawrzyniuk <dominikwawrzyniuk@gmail.com>
2022-05-22 15:04:03 +0200

    Added loading of articles

--- frontend/src/pages/Home/TopNews.tsx
+++ frontend/src/pages/Home/TopNews.tsx
@@ -2,10 +2,16 @@ import { Box, Grid, useMediaQuery } from "@mui/material";
 import Carousel from "../../components/Carousel/Carousel";
 import ArticleTile from "../../components/Tile/ArticleTile";
 import { useTheme } from "@mui/material/styles";
+import { ArticleVM } from "../../api/api";
+import withLoading from "../../fetchData/withLoading";
+import { loadAllArticles } from "../../fetchData/fetchArticles";
 
-export default function TopNews() {
+const NGINX_URL = process.env.REACT_APP_NGINX_CONTENT;
+
+function TopNews({ news }: { news: ArticleVM[] }) {
   const theme = useTheme();
   const matches = useMediaQuery(theme.breakpoints.down("md"));
+
   return (
     <Box className="TopNews" sx={{ mb: 4 }}>
       {matches ? (
@@ -19,67 +25,34 @@ export default function TopNews() {
             responsive: [],
           }}
         >
-          <ArticleTile
-            title="Wiedźmin 3 znowu najlepszy!"
-            src="./images/witcher3.jpg"
-            author="Piotr Nowak"
-            date="11.02.2022"
-          />
-          <ArticleTile
-            title="Gaszenie pożaru w The Sims *Zobacz jak*"
-            src="./images/sims.jpg"
-            author="Bartłomiej Czekaj"
-            date="10.02.2022"
-          />
-          <ArticleTile
-            title="Jak sobie radzi GTA-Online 8 lat po premierze?"
-            src="./images/gta.jpg"
-            author="Bartłomiej Czekaj"
-            date="11.02.2022"
-          />
-          <ArticleTile
-            title="Premiera Daying Light 2 - co przyniesie najnowsza gra polskiej wytwórni?"
-            src="./images/dl2.jpg"
-            author="Bartłomiej Czekaj"
-            date="11.02.2022"
-          />
+          {
+            news.slice(0, 4).map(x =>
+              <ArticleTile
+                title={x.title}
+                src={`${NGINX_URL}/${x.path}/horizontal.png`}
+                author={x.authorName}
+                date={x.publishDate}
+              />
+            )
+          }
         </Carousel>
       ) : (
         <Grid container spacing={3}>
-          <Grid item xs={12} md={8}>
-            <ArticleTile
-              title="Wiedźmin 3 znowu najlepszy!"
-              src="./images/witcher3.jpg"
-              author="Piotr Nowak"
-              date="11.02.2022"
-            />
-          </Grid>
-          <Grid item xs={12} md={4}>
-            <ArticleTile
-              title="Gaszenie pożaru w The Sims *Zobacz jak*"
-              src="./images/sims.jpg"
-              author="Bartłomiej Czekaj"
-              date="10.02.2022"
-            />
-          </Grid>
-          <Grid item xs={12} md={4}>
-            <ArticleTile
-              title="Jak sobie radzi GTA-Online 8 lat po premierze?"
-              src="./images/gta.jpg"
-              author="Bartłomiej Czekaj"
-              date="11.02.2022"
-            />
-          </Grid>
-          <Grid item xs={12} md={8}>
-            <ArticleTile
-              title="Premiera Daying Light 2 - co przyniesie najnowsza gra polskiej wytwórni?"
-              src="./images/dl2.jpg"
-              author="Bartłomiej Czekaj"
-              date="11.02.2022"
-            />
-          </Grid>
+          {
+            news.slice(0, 4).map((x, id) =>
+              <Grid item xs={12} md={12 - id % 2 * 4}>
+                <ArticleTile
+                  title={x.title}
+                  src={`${NGINX_URL}/${x.path}/horizontal.png`}
+                  author={x.authorName}
+                  date={x.publishDate}
+                />
+              </Grid>
+            )
+          }
         </Grid>
       )}
     </Box>
   );
 }
+export default withLoading(TopNews, { news: loadAllArticles })
