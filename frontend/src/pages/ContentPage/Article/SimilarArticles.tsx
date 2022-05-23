7fb2d58b5b45589ce70c6a39185e6b482da9f188
Dominik Wawrzyniuk <dominikwawrzyniuk@gmail.com>
2022-05-23 22:32:25 +0200

    Added image loading to similar articles

--- frontend/src/pages/ContentPage/Article/SimilarArticles.tsx
+++ frontend/src/pages/ContentPage/Article/SimilarArticles.tsx
@@ -1,10 +1,11 @@
-import React from "react";
 import { ArticleSearchInfoVM } from "../../../api/api";
 import Carousel from "../../../components/Carousel/Carousel";
 import ArticleTile from "../../../components/Tile/ArticleTile";
 import { loadSimilarArticles } from "../../../fetchData/fetchArticles";
 import withLoading from "../../../fetchData/withLoading";
 import { convertDate } from "../../../utils/convertDate";
+
+const NGINX_URL = process.env.REACT_APP_NGINX_CONTENT;
 
 function SimilarArticles({ articles }: { articles: ArticleSearchInfoVM }) {
   return (
@@ -17,7 +18,7 @@ function SimilarArticles({ articles }: { articles: ArticleSearchInfoVM }) {
             articleId={a.id}
             key={idx}
             title={a.title}
-            src="https://allegro.stati.pl/AllegroIMG/PRODUCENCI/Bethesda/Fallout%204/GOTY/f2.jpg"
+            src={`${NGINX_URL}/${a.path}/horizontal.png`}
             author={a.authorName}
             date={convertDate(a.publishDate)}
           />
