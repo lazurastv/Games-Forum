979f5df38178d15dc44609d80ae797ef8cf6a31a
Piotr Nowak <npiotrek44@gmail.com>
2022-05-28 16:53:58 +0200

    Added chat better responsivity and date display

--- frontend/src/pages/Chat/Chat.tsx
+++ frontend/src/pages/Chat/Chat.tsx
@@ -1,7 +1,7 @@
-import { darken, IconButton, InputAdornment, styled, TextField } from "@mui/material";
+import { darken, IconButton, InputAdornment, styled, TextField, Typography } from "@mui/material";
 import Container from "@mui/material/Container";
 import { Box } from "@mui/system";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import SockJsClient from "react-stomp";
 import SendIcon from "@mui/icons-material/Send";
 import ChatMessage from "./ChatMessage";
@@ -10,6 +10,7 @@ import { ChatMessageAdd } from "../../api/api/models/ChatMessageAdd";
 import withLoading from "../../fetchData/withLoading";
 import { getChatToken } from "../../fetchData/fetchChat";
 import { useSessionContext } from "../../components/Authentication/SessionContext";
+import SectionHeader from "../../components/SectionHeader";
 const SOCKET_URL = "http://localhost:8080/chat";
 const RECV_PATH = "/topic/message";
 const SEND_PATH = "/app/send";
@@ -18,9 +19,19 @@ const Chat = ({ token }: { token: string }) => {
   const [clientRef, setClientRef] = useState<any>();
   const [messages, setMessages] = useState<ChatMessageVM[]>([]); // add initial loading of messages once implemented
   const [message, setMessage] = useState<string>("");
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView();
+  };
+
   const {
     session: { user },
   } = useSessionContext();
+
   const onConnected = () => {
     console.log("Connected!");
   };
@@ -30,15 +41,11 @@ const Chat = ({ token }: { token: string }) => {
     setMessages([...messages, msg]);
   };
 
-  const sendMessage = () => {
-    clientRef!.sendMessage(SEND_PATH, JSON.stringify({ message: message } as ChatMessageAdd));
-    setMessage("");
-  };
-
   // it must be handled with form if we want message to be sent on enter
   const handleSendMessage = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    sendMessage();
+    clientRef!.sendMessage(SEND_PATH, JSON.stringify({ message: message } as ChatMessageAdd));
+    setMessage("");
   };
   return (
     <Container maxWidth="lg" sx={{ my: 4 }}>
@@ -51,12 +58,14 @@ const Chat = ({ token }: { token: string }) => {
         onMessage={(msg) => onMessageReceived(msg)}
         ref={(client) => setClientRef(client)}
       />
-      <ChatMessagesBox sx={{ mb: 4 }}>
+      <Typography component="h1" variant="h4" color="secondary" sx={{ mb: 1 }}>
+        Chat
+      </Typography>
+      <ChatMessagesBox sx={{ mb: 4, px: 2, py: 4 }}>
         {messages.map((msg, idx) => (
-          <ChatMessage key={idx} isMyMessage={msg.authorId === user?.id}>
-            {msg.message}
-          </ChatMessage>
+          <ChatMessage key={idx} isMyMessage={msg.authorId === user?.id} message={msg} />
         ))}
+        <div ref={messagesEndRef} />
       </ChatMessagesBox>
       <Box component="form" noValidate autoComplete="off" onSubmit={handleSendMessage}>
         <MessageTextField
@@ -81,11 +90,11 @@ const Chat = ({ token }: { token: string }) => {
   );
 };
 export default withLoading(Chat, { token: getChatToken });
+
 const ChatMessagesBox = styled(Box)(({ theme }) => ({
   backgroundColor: theme.palette.primary.main,
-  height: "70vh",
+  height: "65vh",
   borderRadius: 5,
-  padding: `${theme.spacing(2)} ${theme.spacing(4)}`,
   overflowY: "scroll",
 }));
 const MessageTextField = styled(TextField)(({ theme }) => ({
--- frontend/src/pages/Chat/ChatMessage.tsx
+++ frontend/src/pages/Chat/ChatMessage.tsx
@@ -1,36 +1,80 @@
-import { Box } from "@mui/material";
+import { Avatar, Box, Typography } from "@mui/material";
 import { darken, styled } from "@mui/system";
 import React from "react";
+import { ChatMessageVM } from "../../api/api/models/ChatMessageVM";
+const NGINX_URL = process.env.REACT_APP_NGINX_USER;
+
 interface ChatMessageProps {
   isMyMessage?: boolean;
-  children?: React.ReactNode;
-  message?: {
-    authorName: string;
-    message?: string;
-    profilePicturePath?:string
-  };
+  message: ChatMessageVM;
 }
-export default function ChatMessage({ isMyMessage, children }: ChatMessageProps) {
+export default function ChatMessage({ isMyMessage, message }: ChatMessageProps) {
+  const hour = message.publishDate.getHours();
+  const minute = message.publishDate.getMinutes();
+
   return (
-    <Box>
-      {isMyMessage ? <MyMessageBox>{children}</MyMessageBox> : <OthersMessageBox>{children}</OthersMessageBox>}
+    <Box
+      sx={{
+        display: "flex",
+        mb: 4,
+        // justifyContent: isMyMessage ? "flex-end" : "flex-start",
+        flexDirection: "column",
+        alignItems: isMyMessage ? "flex-end" : "flex-start",
+        gap: 1,
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          gap: 2,
+          alignItems: "center",
+          flexDirection: isMyMessage ? "row-reverse" : "row",
+          mb: 1,
+        }}
+      >
+        <Avatar
+          sx={{ width: 32, height: 32 }}
+          src={`${NGINX_URL}/${message.profilePicturePath}/profile.png`}
+          alt={message.authorName}
+        />
+        <Box sx={{ display: "flex", flexDirection: "column", alignItems: isMyMessage ? "flex-end" : "flex-start" }}>
+          <Typography color="secondary">{message.authorName}</Typography>
+          <Typography variant="caption" sx={{ color: "text.secondary" }}>{`${hour}:${minute}`}</Typography>
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          maxWidth: {
+            xs: "100%",
+            md: 700,
+          },
+          display: "flex",
+          px: {
+            xs: 0,
+            md: 5,
+          },
+        }}
+      >
+        {isMyMessage ? (
+          <MyMessageBox>{message.message}</MyMessageBox>
+        ) : (
+          <OthersMessageBox>{message.message}</OthersMessageBox>
+        )}
+      </Box>
     </Box>
   );
 }
 const MessageBox = styled(Box)(({ theme }) => ({
   wordWrap: "break-word",
   width: "fit-content",
-  maxWidth: "70%",
+  maxWidth: "100%",
   padding: `${theme.spacing(1.2)} ${theme.spacing(2)}`,
-  marginBottom:theme.spacing(1),
   borderRadius: 25,
   color: theme.palette.staticText.primary,
 }));
 const MyMessageBox = styled(MessageBox)(({ theme }) => ({
-  marginLeft: "auto",
   backgroundColor: theme.palette.secondary.main,
 }));
 const OthersMessageBox = styled(MessageBox)(({ theme }) => ({
-  marginRight: "auto",
   backgroundColor: darken(theme.palette.primary.light, 0.4),
 }));
