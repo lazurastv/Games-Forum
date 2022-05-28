c96ecca76e433d1908fddf18912d259f1098c5dc
Dominik Wawrzyniuk <dominikwawrzyniuk@gmail.com>
2022-05-28 22:56:42 +0200

    Added date field to comment and commentVM

--- backend/src/main/java/com/gamebroadcast/forum/interaction/comment/models/CommentVM.java
+++ backend/src/main/java/com/gamebroadcast/forum/interaction/comment/models/CommentVM.java
@@ -1,18 +1,21 @@
 package com.gamebroadcast.forum.interaction.comment.models;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class CommentVM {
     public Long id;
     public Long contentId;
     public String authorName;
+    public Date publishDate;
     public String comment;
 
     public CommentVM(Comment comment) {
         this.id = comment.getId();
         this.contentId = comment.getContent().getId();
         this.authorName = comment.getAuthor().getUsername();
+        this.publishDate = comment.getPublishDate();
         this.comment = comment.getComment();
     }
 
