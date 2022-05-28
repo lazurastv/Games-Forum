6936d5175d74029c48f5b92884f808c8234ce660
Dominik Wawrzyniuk <dominikwawrzyniuk@gmail.com>
2022-05-28 13:51:12 +0200

    Added loading new messages to list

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -1,6 +1,8 @@
 import Container from '@mui/material/Container';
 import React, { useState } from 'react';
 import SockJsClient from 'react-stomp';
+import { ChatMessageAdd } from '../api/api/models/ChatMessageAdd';
+import { ChatMessageVM } from '../api/api/models/ChatMessageVM';
 import { getChatToken } from '../fetchData/fetchChat';
 import withLoading from '../fetchData/withLoading';
 
@@ -10,19 +12,20 @@ const SEND_PATH = '/app/send';
 
 const Chat = ({ token }: { token: string }) => {
   const [clientRef, setClientRef] = useState<any>();
+  const [messages, setMessages] = useState<ChatMessageVM[]>([]);  // add initial loading of messages once implemented
   const [message, setMessage] = useState<string>("");
 
   const onConnected = () => {
     console.log("Connected!")
   }
 
-  const onMessageReceived = (msg) => {
-    console.log("I received:");
-    console.log(msg);
+  const onMessageReceived = (msg: ChatMessageVM) => {
+    msg.publishDate = new Date(msg.publishDate);
+    setMessages([...messages, msg]);
   }
 
   const sendMessage = () => {
-    clientRef!.sendMessage(SEND_PATH, JSON.stringify({ message: message }));
+    clientRef!.sendMessage(SEND_PATH, JSON.stringify({ message: message } as ChatMessageAdd));
     setMessage("");
   }
 
@@ -39,6 +42,11 @@ const Chat = ({ token }: { token: string }) => {
       />
       <input value={message} onChange={(evt) => setMessage(evt.target.value)}></input>
       <button onClick={sendMessage}>Wyślij</button>
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        {
+          messages.map(x => <p key={x.publishDate.toISOString()} style={{ color: 'black', backgroundColor: 'white' }}>{x.authorId + ": " + x.message}</p>)
+        }
+      </div>
     </Container>
   );
 };
