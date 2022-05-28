c541385e638de723aa1f8dbf39e8879ea5468fc6
Prz-mek <kwasniewski.pj@gmail.com>
2022-05-28 16:13:31 +0200

    cleaning

--- frontend/src/pages/ContentCreate/CreateArticle/CreateArticle.tsx
+++ frontend/src/pages/ContentCreate/CreateArticle/CreateArticle.tsx
@@ -62,7 +62,6 @@ export default function CreateArticle() {
             variant="contained"
             color="secondary"
             size="large"
-            onClick={() => setIsOpen(true)}
           >
             Zapisz
           </Button>
