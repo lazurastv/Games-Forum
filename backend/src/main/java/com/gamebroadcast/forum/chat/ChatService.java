3d940b5c66bfd8baf3966e987dad0131cf4501d8
Norsien <smola27ms@gmail.com>
2022-05-28 18:16:34 +0200

    no contoller - cannot test

--- backend/src/main/java/com/gamebroadcast/forum/chat/ChatService.java
+++ backend/src/main/java/com/gamebroadcast/forum/chat/ChatService.java
@@ -1,5 +1,6 @@
 package com.gamebroadcast.forum.chat;
 
+import java.util.Date;
 import java.util.List;
 
 import com.gamebroadcast.forum.chat.models.ChatMessage;
@@ -15,6 +16,19 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ChatService {
     public final ChatRepository chatRepository;
+    private Date lastClear;
+
+    public List<ChatMessageVM> getAll() {
+
+        Date currentDate = new Date();
+        Date yesterday = new Date(currentDate.getTime() - 1000 * 60 * 60 * 24);
+        if (lastClear == null || lastClear.before(yesterday)) {
+            clearOldMessages();
+        }
+
+        List<ChatMessage> chatMessages = chatRepository.findAll();
+        return ChatMessageVM.toChatMessageVMList(chatMessages);
+    }
 
     public void add(ChatMessageAdd chatMessageAdd) throws IllegalStateException {
         ChatMessage chatMessage = chatMessageAdd.toChatMessage();
@@ -27,13 +41,14 @@ public class ChatService {
         chatRepository.delete(chatMessage);
     }
 
-    public List<ChatMessageVM> getAll() {
-        List<ChatMessage> chatMessages = chatRepository.findAll();
-        return ChatMessageVM.toChatMessageVMList(chatMessages);
-    }
 
     public ChatMessage getChatMessage(Long id) {
         return chatRepository.findById(id)
             .orElseThrow(() -> new ItemNotFoundException("Message", id));
+    }
+
+    private void clearOldMessages() {
+        chatRepository.deleteByPublishDateLessThan(lastClear);
+        lastClear = new Date();
     }
 }
