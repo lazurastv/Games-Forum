3235795f07df5c3b469400b6ac128343d692fba4
Dominik Wawrzyniuk <dominikwawrzyniuk@gmail.com>
2022-06-13 18:24:57 +0200

    Added loading users to list

--- frontend/src/pages/UserList/Users.tsx
+++ frontend/src/pages/UserList/Users.tsx
@@ -1,5 +1,11 @@
+
+import { Box, Container } from "@mui/material";
 import { UserVM } from "../../api/api";
+import Filter from "../../components/Filters/Filter/Filter";
+import { loadAllUsers } from "../../fetchData/fetchUser";
 import withLoading from "../../fetchData/withLoading";
+import useFilterData from "../../hooks/useFilterData";
+import UserItem from "./UserItem";
 
 const NGINX_URL = process.env.REACT_APP_NGINX_USER;
 
@@ -7,41 +13,30 @@ interface UsersProps {
   users: UserVM[];
 }
 
-const Users = () => {//(props: UsersProps) => {
-  // const { Users, edit, userName } = props;
-  // const filter = useFilterData(Users, userName);
-  // const { displayAlert } = useAlert();
-  // const handleDeleteUser = (id: number, title: string) => {
-  //   deleteUser(id)
-  //     .then(() => displayAlert(`Pomyślenie usunięto "${title}" `))
-  //     .catch((err) => displayAlert(`Błąd podczas usuwania "${title}" `, true));
-  //   if (props.setReload) {
-  //     props.setReload((r) => r + 1);
-  //   }
-  // };
-  // // string | undefined as string - czy to jest poprawnie czy dodać undefined do interfejsu, czy te dane mogą być undefined?
-  // return (
-  //   <Container maxWidth="xl">
-  //     <Filter sliderLabel="DATA PUBLIKACJI:" data={Users} {...filter.filterControl} />
-  //     <Box sx={{ minHeight: "100vh" }}>
-  //       {filter.Feedback
-  //         ? filter.Feedback
-  //         : filter.data.map((a: any, idx: any) => (
-  //           <EditMenuSupply key={idx} edit={edit} onDelete={() => handleDeleteUser(a.id, a.title)}>
-  //             <UserItem
-  //               UserId={a.id}
-  //               content={a.introduction}
-  //               date={convertDate(a.publishDate)}
-  //               title={a.title as string}
-  //               author={a.authorName as string}
-  //               image={`${NGINX_URL}/${a.path}/horizontal.png`}
-  //             />
-  //           </EditMenuSupply>
-  //         ))}
-  //     </Box>
-  //   </Container>
-  // );
-  return <div>Hi</div>
+const Users = (props: UsersProps) => {
+  const { users } = props;
+  let username: any = undefined;
+  const filter = useFilterData(users, username);
+
+  return (
+    <Container maxWidth="xl">
+      <Filter sliderLabel="DATA PUBLIKACJI:" data={users} {...filter.filterControl} />
+      <Box sx={{ minHeight: "100vh" }}>
+        {filter.Feedback
+          ? filter.Feedback
+          : filter.data.map((a: any, idx: any) => (
+            <UserItem
+              key={idx}
+              id={a.id}
+              username={a.username}
+              shortDescription={a.shortDescription}
+              image={`${NGINX_URL}/${a.profilePicturePath}/profile.png`}
+              email={a.email}
+              role={a.role}
+            />
+          ))}
+      </Box>
+    </Container>
+  );
 };
-// export default withLoading(Users, { Users: loadAllUsers });
-export default Users;
+export default withLoading(Users, { users: loadAllUsers });
