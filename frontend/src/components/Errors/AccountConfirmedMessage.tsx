bb546f062c5a6600c544051d918419a4db975760
Prz-mek <kwasniewski.pj@gmail.com>
2022-06-14 10:39:18 +0200

    site content

---
+++ frontend/src/components/Errors/AccountConfirmedMessage.tsx
@@ -0,0 +1,31 @@
+import { Box } from "@mui/system";
+import { Button, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import Stack from '@mui/material/Stack';
+
+export default function AccountCreatedMessage() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "200px",
+        gap: 1,
+      }}
+    >
+      <Stack spacing={2}>
+        <Typography sx={{ fontSize: 28 }}>Twoje konto zostało aktywowane</Typography>
+        <Box>
+            Gratulacje Twoja rejestracja jest kompletna!
+        </Box>
+        <Link to={`/`}>
+          <Button sx={{ minWidth: "32px" }} size="large" variant="contained" color="secondary">
+            Do strony głównej
+          </Button>
+        </Link>
+        
+      </Stack>
+    </Box>
+  );
+}
